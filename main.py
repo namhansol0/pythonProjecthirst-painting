4af2435e592e4bae606337f1d355fed60ab6c35e
gksthfdmstoaa <gksthfdmstoaa@naver.com>
2023-04-11 21:23:47 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import turtle
+import turtle as turtle_module
+import random
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+color_list = [(245, 248, 253), (244, 235, 46), (196, 12, 34), (221, 159, 69), (43, 80, 178), (238, 39, 143), (40, 215, 68), (238, 229, 5), (30, 40, 154), (23, 147, 26), (207, 74, 22), (202, 34, 91), (186, 16, 9), (19, 18, 42), (216, 141, 191), (57, 15, 10), (88, 8, 28), (227, 161, 9), (78, 212, 157), (67, 73, 221), (13, 95, 61), (78, 194, 225), (239, 158, 215), (94, 233, 204), (220, 76, 48), (15, 67, 46), (7, 226, 238)]
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+tim.fd(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.fd(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.fd(50)
+        tim.setheading(180)
+        tim.fd(500)
+        tim.setheading(0)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
